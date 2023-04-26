186bba94a12761ad4dbbc83f0efc76b3acd80c38
Andrea Ghidara <Ghidara.andrea.99@gmail.com>
2023-04-26 16:54:24 +0200

    change a variable

--- assest/js/main.js
+++ assest/js/main.js
@@ -3,11 +3,11 @@ const btnCreateTable = document.querySelector("#createTable");
 const levelDifficuly = document.querySelector("#difficultyLevel");
 
 let difficulty;
+let numberSquere;
 
 function clicked(box) {
     box.classList.add("blue");
 }
-
 
 
 btnCreateTable.addEventListener("click", function(){
@@ -15,28 +15,28 @@ btnCreateTable.addEventListener("click", function(){
     difficulty = levelDifficuly.value;
 
     if (difficulty == 1) {
-        difficulty = 100;
+        numberSquere = 100;
     }
     
     if (difficulty == 2) {
-        difficulty = 81;
+        numberSquere = 81;
     }
 
     if (difficulty == 3) {
-        difficulty = 49;
+        numberSquere = 49;
     }
 
-    for (let i = 1; i < difficulty + 1; i++) {
+    for (let i = 1; i < numberSquere + 1; i++) {
         let square = document.createElement("div");
         square.classList.add("square");
         square.innerText = i;
-        if (difficulty == 100) {
+        if (difficulty == 1) {
             square.classList.add("easySquere");
         }
-        if (difficulty == 81) {
+        if (difficulty == 2) {
             square.classList.add("mediumSquere");
         }
-        if (difficulty == 49) {
+        if (difficulty == 3) {
             square.classList.add("hardSquere");
         }
         container.append(square)
